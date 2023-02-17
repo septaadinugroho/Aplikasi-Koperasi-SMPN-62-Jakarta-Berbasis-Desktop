d362d797991da02167f4adca7ccc07ec3399a87e
Septa Adi Nugroho <110009347+septaadinugroho@users.noreply.github.com>
2023-02-17 23:26:40 +0700

    Add files via upload

---
+++ Koneksi.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konfigurasi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Septa
+ */
+public class Koneksi {
+     public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost/smpn62";
+        String user = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection c = Koneksi.getConnection();
+            System.out.println(String.format("Connected to database %s " + "successfully.", c.getCatalog()));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
